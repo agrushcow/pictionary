073950ad01d900c348552e5a663ec0c94d328107
ADAM GRUSHCOW <adamgrushcow@gmail.com>
2016-09-13 11:38:17 -0700

    Fixed error in initial file structure where socket was not being declared

--- public/main.js
+++ public/main.js
@@ -1,75 +1,26 @@
+var pictionary = function() {
+    var canvas, context;
+
+    var draw = function(position) {
+        context.beginPath();
+        context.arc(position.x, position.y,
+                         6, 0, 2 * Math.PI);
+        context.fill();
+    };
+
+    canvas = $('canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+    canvas.on('mousemove', function(event) {
+        var offset = canvas.offset();
+        var position = {x: event.pageX - offset.left,
+                        y: event.pageY - offset.top};
+        draw(position);
+    });
+};
+
 $(document).ready(function() {
     var socket = io();
-    var input = $('input');
-    var messages = $('#messages');
-    var typing = $('#typing');
-    var connections = $('#connections span');
-    var clickCount = 0;
-
-    var addMessage = function(message) {
-        messages.append('<div>' + message + '</div>');
-        typing.text("");
-    };
-
-    var updateConnections = function(connectionCount) {
-      connections.html(connectionCount);
-      console.log(connectionCount);
-      //still only updates on refresh, have questions about actions onload/close
-    };
-
-    var addUsername = function(username) {
-      messages.append(username);
-    };
-
-    var userType = function(msg) {
-      typing.html('<div>' + msg + '</div>');
-      //add timeout
-    }
-
-/*------------------  jQuery  ---------------------*/
-    input.keypress(function(event) {
-      if(event.keyCode == 13) {
-        if(!socket.username) {
-          //user is not signed in yet
-          socket.emit('username', input.val());
-          socket.username = input.val();
-        } else {
-          //user is signed in
-          socket.emit('message', input.val());
-          addMessage(socket.username + ": " + input.val());
-        }
-        input.val("");
-        typing.text("");
-      } else {
-        if (socket.username) {
-            socket.emit('typing');
-        }
-      }
-    });
-
-        // if(clickCount == 0) {
-        //   if(event.keycode = 13) {
-        //     addUsername(username);
-        //     clickCount++;
-        //   }
-        // } else {
-        //   if (event.keyCode != 13) {
-        //       return;
-        //   }
-        //   var message = input.val();
-        //
-        //   if (socket.username) {
-        //     addMessage(socket.username + ": " + message);
-        //     socket.emit('message', message);
-        //   } else {
-        //     socket.emit('username', message);
-        //     socket.username = message;
-        //   }
-        //   input.val('');
-        // }
-
-socket.on('message', addMessage);
-socket.on('typing', userType);
-socket.on('total', updateConnections);
-socket.on('username', addUsername);
+    pictionary();
 });
